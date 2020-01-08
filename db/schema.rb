ab46aa23fd502224da19a44ff334763c46bb9e1e
Andy Gao <yidiangao23@gmail.com>
2020-01-08 17:52:59 +0800

    [Andy Gao] add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_062941) do
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.string "email", limit: 191, default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "token", limit: 191
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
