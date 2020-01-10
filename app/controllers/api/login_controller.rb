36af1737c47506521c067641282ce9f83c92bb59
Andy Gao <yidiangao23@gmail.com>
2020-01-10 19:17:25 +0800

    [Andy Gao] change dir

--- app/controllers/api/login_controller.rb
+++ app/controllers/api/login_controller.rb
@@ -1,6 +1,8 @@
 class Api::LoginController < ApplicationController
   include RenderJSON
 
+  protect_from_forgery unless: -> { request.format.json? }
+
   def create
     user = User.find_by(email: params[:email])
     if user.try(:valid_password?, params[:password])
